66ff30066f4248f84edf9ea8efcc17a43192fe81
PinskyRobin <xu568059888@163.com>
2022-04-15 12:23:57 +0800

    finish rough arch

--- exp2/tensorflow/lab2_flower_classify.py
+++ exp2/tensorflow/lab2_flower_classify.py
@@ -2,10 +2,12 @@ import glob                # 用于查询符合特定规则的文件路径名
 import os                  # 处理文件和目录
 import cv2                 # 用于图像处理
 import tensorflow as tf
-from tensorflow.keras import layers, optimizers, datasets, Sequential  #从tensorflow.keras模块下导入layers，optimizers, datasets, Sequential等方法
-import numpy as np                #导入numpy数据库
-import matplotlib.pyplot as plt   #导入matplotlib.pyplot模块，主要用于展示图像
-from sklearn.model_selection import train_test_split   #从sklearn.model_selection模块导入train_test_split方法，用于拆分数据集
+# 从tensorflow.keras模块下导入layers，optimizers, datasets, Sequential等方法
+from tensorflow.keras import layers, optimizers, datasets, Sequential
+import numpy as np  # 导入numpy数据库
+import matplotlib.pyplot as plt  # 导入matplotlib.pyplot模块，主要用于展示图像
+from sklearn.model_selection import train_test_split
+from torch import eig  # 从sklearn.model_selection模块导入train_test_split方法，用于拆分数据集
 
 # 所用第三方包的安装方式，安装前更新pip:
 # python -m pip install --upgrade pip
@@ -14,81 +16,144 @@ from sklearn.model_selection import train_test_split   #从sklearn.model_selecti
 # pip install matplotlib
 # pip install sklearn
 
-path = './flower_photos/'   # 数据集的相对地址，改为你自己的，建议将数据集放入代码文件夹下
+class params:
+    def __init__(self):
+        cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        self.path_train = os.path.join(cur_dir, 'flower_photos/')
+        self.path_test = os.path.join(cur_dir, 'TestImages/')
+        self.wide = 128
+        self.height = 128
+        self.channel = 3
+        self.lr = 0.0001
+        self.epochs = 100
+        self.batch_size = 64
+        self.split = 0.2
 
-# Todo 对图片进行缩放，统一处理为大小为w*h的图像，具体数据需自己定
-w = 10       #设置图片宽度
-h = 10       #设置图片高度
-c = 3        #设置图片通道为3
+_ = params()
 
+# API for reading image from disk
+def read_img(path, wide, height):
 
-def read_img(path):                                                    # 定义函数read_img，用于读取图像数据，并且对图像进行resize格式统一处理
-    cate=[path+x for x in os.listdir(path) if os.path.isdir(path+x)]  # 创建层级列表cate，用于对数据存放目录下面的数据文件夹进行遍历，os.path.isdir用于判断文件是否是目录，然后对是目录的文件(os.listdir(path))进行遍历
-    imgs=[]                                                            # 创建保存图像的空列表
-    labels=[]                                                          # 创建用于保存图像标签的空列表
-    for idx,folder in enumerate(cate):                                # enumerate函数用于将一个可遍历的数据对象组合为一个索引序列，同时列出数据和下标,一般用在for循环当中
-        for im in glob.glob(folder+'/*.jpg'):                         # 利用glob.glob函数搜索每个层级文件下面符合特定格式“/*.jpg”的图片，并进行遍历
-            #print('reading the images:%s'%(im))                      # 遍历图像的同时，打印每张图片的“路径+名称”信息
-            img=cv2.imread(im)                                        # 利用cv2.imread函数读取每一张被遍历的图像并将其赋值给img
-            img=cv2.resize(img,(w,h))                                 # 利用cv2.resize函数对每张img图像进行大小缩放，统一处理为大小为w*h的图像
-            imgs.append(img)                                          # 将每张经过处理的图像数据保存在之前创建的imgs空列表当中
-            labels.append(idx)                                        # 将每张经过处理的图像的标签数据保存在之前创建的labels列表当中
-    return np.asarray(imgs,np.float32),np.asarray(labels,np.int32)   # 利用np.asarray函数对生成的imgs和labels列表数据进行转化，之后转化成数组数据（imgs转成浮点数型，labels转成整数型）
+    cate = [path + x for x in os.listdir(path) if os.path.isdir(path + x)]
+    imgs = []
+    labels = []
 
+    for idx, folder in enumerate(cate):
+        for im in glob.glob(folder + '/*.jpg'):
+            # print('reading the images:%s'%(im))
+            img = cv2.imread(im)
+            img = cv2.resize(img, (wide, height))
+            imgs.append(img)
+            labels.append(idx)
 
-data, label = read_img(path)                                           # 将read_img函数处理之后的数据定义为样本数据data和标签数据label
-print("shape of data:",data.shape)                                     # 查看样本数据的大小
-print("shape of label:",label.shape)                                   # 查看标签数据的大小
+    return np.asarray(imgs, np.float32), np.asarray(labels, np.int32)
 
+# Read and process data
+def preprocess(path_train, wide, height, split=0.2):
+    data, label = read_img(path_train, wide, height)
+    print("shape of data:", data.shape)
+    print("shape of label:", label.shape)
 
-seed = 109             # 设置随机数种子，即seed值
-np.random.seed(seed)   # 保证生成的随机数具有可预测性,即相同的种子（seed值）所产生的随机数是相同的
+    seed = 109
+    np.random.seed(seed)
 
-(x_train, x_val, y_train, y_val) = train_test_split(data, label, test_size=0.20, random_state=seed) #拆分数据集一部分为训练集一部分为验证集，拆分比例可调整
-x_train = x_train / 255  #训练集图片标准化
-x_val = x_val / 255      #测试集图片标准化
+    (x_train, x_val, y_train, y_val) = train_test_split(
+        data, label, test_size=split, random_state=seed)
 
-flower_dict = {0:'bee',1:'blackberry',2:'blanket',3:'bougainvillea',4:'bromelia',5:'foxglove'} #创建图像标签列表
+    # Standardize the data
+    x_train = x_train / 255
+    x_val = x_val / 255
 
-# Todo 自行实现模型结构
-model = Sequential([])
+    flower_dict = {0: 'bee', 1: 'blackberry', 2: 'blanket',
+                   3: 'bougainvillea', 4: 'bromelia', 5: 'foxglove'}
 
-# Todo 可调整超参数lr，可选择其他优化器
-opt = optimizers.Adam(learning_rate=0.0001)   #使用Adam优化器，优化模型参数。lr(learning rate, 学习率)
-
-#编译模型以供训练。使用多分类损失函数'sparse_categorical_crossentropy'，使用metrics=['accuracy']即评估模型在训练和测试时的性能的指标，使用的准确率。
-# Todo可选择其他损失函数
-model.compile(optimizer=opt,
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
-
-
-# Todo 可调整超参数
-#训练模型，决定训练集和验证集，batch size：进行梯度下降训练模型时每个batch包含的样本数。
-#verbose：日志显示，0为不在标准输出流输出日志信息，1为输出进度条记录，2为每个epoch输出一行记录
-model.fit(x_train, y_train, epochs=30, validation_data=(x_val, y_val),batch_size=200, verbose=2)
-#输出模型的结构和参数量
-model.summary()
-
-path_test = './TestImages/' # 测试图像的地址 （改为自己的）
-
-imgs=[]                                                 # 创建保存图像的空列表
-for im in glob.glob(path_test+'/*.jpg'):               # 利用glob.glob函数搜索每个层级文件下面符合特定格式“/*.jpg”进行遍历
-    #print('reading the images:%s'%(im))                # 遍历图像的同时，打印每张图片的“路径+名称”信息
-    img=cv2.imread(im)                                  # 利用io.imread函数读取每一张被遍历的图像并将其赋值给img
-    img=cv2.resize(img,(w,h))                           # 利用cv2.resize函数对每张img图像进行大小缩放，统一处理为大小为w*h的图像
-    imgs.append(img)                                    # 将每张经过处理的图像数据保存在之前创建的imgs空列表当中
-imgs = np.asarray(imgs,np.float32)                      # 利用np.asarray()函数对imgs进行数据转换
-print("shape of data:",imgs.shape)
+    return x_train, x_val, y_train, y_val, flower_dict
 
 
-prediction = np.argmax(model.predict(imgs), axis=1) #将图像导入模型进行预测
-# prediction = model.predict_classes(imgs)      #将图像导入模型进行预测，旧版本tf可以使用该接口，最新版本已废弃
-#绘制预测图像
-for i in range(np.size(prediction)):
-    #打印每张图像的预测结果
-    print("第",i+1,"朵花预测:"+flower_dict[prediction[i]])  # flower_dict:定义的标签列表，prediction[i]：预测的结果
-    img = plt.imread(path_test+"test"+str(i+1)+".jpg")      # 使用imread()函数读入对应的图片
-    #img = plt.imread(path_test)
-    plt.imshow(img)              #展示图片
-    plt.show()                   #显示图片
+def build():
+    model = Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+              input_shape=(128, 128, 3)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Flatten())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(512, activation='relu'))
+    model.add(layers.Dense(6, activation='softmax'))
+    return model
+
+
+def train(model, x_train, y_train, x_val, y_val, lr, epochs, batch_size):
+
+    opt = optimizers.Adam(learning_rate=lr)
+
+    model.compile(optimizer=opt,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['acc'])
+
+    history = model.fit(x_train, y_train,
+                        epochs=epochs,
+                        validation_data=(x_val, y_val),
+                        batch_size=batch_size,
+                        verbose=2)
+
+    model.summary()
+
+    model.save('flower_model.h5')
+
+    # draw the train and validation loss
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+    return model
+
+
+def predict(model, path_test, wide, height, flower_dict):
+    imgs = []
+    for im in glob.glob(path_test + '/*.jpg'):
+        img = cv2.imread(im)
+        img = cv2.resize(img, (wide, height))
+        imgs.append(img)
+    imgs = np.asarray(imgs, np.float32)
+    print("shape of data:", imgs.shape)
+
+    prediction = np.argmax(model.predict(imgs), axis=1)
+    
+    # draw the prediction
+    for i in range(np.size(prediction)):
+        print("第", i+1, "朵花预测:"+flower_dict[prediction[i]])
+        img = plt.imread(path_test+"test"+str(i+1) +
+                         ".jpg")
+        plt.imshow(img)
+        plt.show()
+
+
+if __name__ == '__main__':
+    
+    x_train, x_val, y_train, y_val, flower_dict = preprocess(
+        _.path_train, _.wide, _.height, _.split)
+
+    model = build()
+
+    model = train(model, x_train, y_train, x_val,
+                  y_val, _.lr, _.epochs, _.batch_size)
+
+    predict(model, _.path_test, _.wide, _.height, flower_dict)
