6b90ec86b5de6d00a7468748be84d37e8ed17b76
PinskyRobin <xu568059888@163.com>
2022-04-23 18:02:38 +0800

    train part needs to be modified

--- exp2/tensorflow/lab2_flower_classify.py
+++ exp2/tensorflow/lab2_flower_classify.py
@@ -1,37 +1,37 @@
 import glob
 import os
 import cv2
-import tensorflow as tf
-
-###############################################################################
-# pip install pakages are listed in requirements.txt #
-###############################################################################
-
-
-# If following pakages cannot be recognized,
-# please use "tensorflow.keras.xxx" instead
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, Dropout
-from tensorflow.python.keras.callbacks import LearningRateScheduler, EarlyStopping
-from tensorflow.python.keras.regularizers import l2
-
-# If following pakages cannot be recognized,
-# please use please use "tensorflow.keras.optimizer" instead
-from tensorflow.python.keras.optimizer_v2.adam import Adam
-from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
-
 import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
+###############################################################################
+# pip install packages are listed in requirements.txt #
+###############################################################################
+
+# If following packages cannot be recognized,
+# please use "tensorflow.keras.xxx" instead
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.callbacks import EarlyStopping
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.callbacks import EarlyStopping
+
+# If following packages cannot be recognized,
+# please use please use "tensorflow.keras.optimizer" instead
+from tensorflow.python.keras.optimizer_v2.adam import Adam
+from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
+# from tensorflow.python.keras.optimizer.adam import Adam
+# from tensorflow.python.keras.optimizer.gradient_descent import SGD
+
 # Fix prediction failure in macOS environment
-# If this line raise error, please just comment it
+# If this line raise any error, please just COMMENT it
 tf.executing_eagerly()
 
 
-# All hyper-parameters used in this program are listed in class params
+# All hyper-parameters used in this program are listed in class Parameters
 # You can refer any of them by using "_.xxx"
-class params:
+class Parameters:
     def __init__(self):
         cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
         self.path_train = os.path.join(cur_dir, 'flower_photos/')
@@ -39,17 +39,18 @@ class params:
         self.wide = 224
         self.height = 224
         self.channel = 3
-        self.lr = 0.1
-        self.epochs = 100
+        self.lr = 0.001
+        self.epochs = 20
         self.dropout = 0.5
         self.batch_size = 64
-        self.split = 0.2
-        
+        self.split = 0.3
+        self.seed = 42
+
         self.flower_dict = {0: 'bee', 1: 'blackberry', 2: 'blanket',
-                    3: 'bougainvillea', 4: 'bromelia', 5: 'foxglove'}
+                            3: 'bougainvillea', 4: 'bromelia', 5: 'foxglove'}
 
 
-_ = params()
+_ = Parameters()
 
 
 # API for reading image from disk
@@ -69,17 +70,13 @@ def read_img(path, wide, height):
     return np.asarray(imgs, np.float32), np.asarray(labels, np.int32)
 
 
-# Read and process data
 def preprocess():
     data, label = read_img(_.path_train, _.wide, _.height)
     print("shape of data:", data.shape)
     print("shape of label:", label.shape)
 
-    seed = 109
-    np.random.seed(seed)
-
     (x_train, x_val, y_train, y_val) = train_test_split(
-        data, label, test_size=_.split, random_state=seed)
+        data, label, test_size=_.split, random_state=_.seed)
 
     # Standardize the data
     x_train = x_train / 255
@@ -89,35 +86,16 @@ def preprocess():
 
 
 def build():
-    model = Sequential([
-        Conv2D(32, kernel_size=[5, 5], padding="same", activation=tf.nn.relu),
-        MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
-        Dropout(0.35),
-
-        Conv2D(64, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
-        Conv2D(64, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
-        MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
-        Dropout(0.35),
-
-        Conv2D(128, kernel_size=[3, 3], padding="same", activation=tf.nn.relu),
-        MaxPool2D(pool_size=[2, 2], strides=2, padding='same'),
-        Dropout(0.35),
-        
-        Flatten(),
-        Dense(512, activation=tf.nn.relu),
-        Dense(256, activation=tf.nn.relu),
-        
-        Dense(6, activation='softmax')
-    ])
-
+    model = Sequential()
     return model
 
 
 def train(model, x_train, y_train, x_val, y_val):
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=10, restore_best_weights=True)
 
-    opt = Adam(learning_rate=0.001)
-    sgd = SGD(lr=_.lr, momentum=0.9, nesterov=True)
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5, restore_best_weights=True)
+
+    opt = Adam(learning_rate=_.lr)
+    # sgd = SGD(lr=_.lr, momentum=0.9, nesterov=True)
     model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['acc'])
 
     history = model.fit(x_train, y_train,
@@ -129,7 +107,7 @@ def train(model, x_train, y_train, x_val, y_val):
 
     model.summary()
 
-    model.save('flower_model.h5')
+    model.save_weights('flower_model.pkl')
 
     # Draw the train and validation loss
     acc = history.history['acc']
@@ -149,7 +127,6 @@ def train(model, x_train, y_train, x_val, y_val):
     plt.show()
 
     return model
-
 
 def predict(model):
     imgs = []
