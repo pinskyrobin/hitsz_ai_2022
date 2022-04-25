41ba27b94a4e3a0fbda106201433c809825d8c83
PinskyRobin <xu568059888@163.com>
2022-04-25 12:09:57 +0800

    train file needed to upload to ModelArt

--- exp2/tensorflow/lab2_flower_classify_tf_modelarts.py
+++ exp2/tensorflow/lab2_flower_classify_tf_modelarts.py
@@ -14,7 +14,34 @@ from sklearn.model_selection import train_test_split   #从sklearn.model_selecti
 # pip install matplotlib
 # pip install sklearn
 
+
 import argparse
+
+# If following packages cannot be recognized,
+# please use "tensorflow.keras.xxx" instead
+
+# from tensorflow.python.keras.models import Sequential
+# from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+# from keras_preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+# If following packages cannot be recognized,
+# please use please use "tensorflow.keras.optimizers" instead
+
+# from tensorflow.python.keras.optimizer_v2.adam import Adam
+
+from tensorflow.keras.optimizers import Adam
+
+# Fix prediction failure in macOS environment
+# If this line raise any error, please just COMMENT it
+
+# tf.executing_eagerly()
+
+
 # 创建解析
 parser = argparse.ArgumentParser(description="train flower classify",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -25,62 +52,131 @@ parser.add_argument('--data_url', type=str, help='the training data')
 # 解析参数
 args, unkown = parser.parse_known_args()
 
-path = args.data_url
-model_path = args.train_url
-
 #path = './flower_photos/'   # 数据集的相对地址，改为你自己的，建议将数据集放入代码文件夹下
 
-# Todo 对图片进行缩放，统一处理为大小为w*h的图像，具体数据需自己定
-w = 10       #设置图片宽度
-h = 10       #设置图片高度
-c = 3        #设置图片通道为3
+# All hyper-parameters used in this program are listed in class Parameters
+# You can refer any of them by using "_.xxx"
+class Parameters:
+    def __init__(self):
+        self.path_train = args.data_url
+        self.path_model = args.train_url
+        self.wide = 100
+        self.height = 100
+        self.channel = 3
+        self.lr = 0.001
+        self.epochs = 90
+        self.batch_size = 32
+        self.split = 0.20
+        self.verbose = 2
+
+        self.flower_dict = {0: 'bee', 1: 'blackberry', 2: 'blanket',
+                            3: 'bougainvillea', 4: 'bromelia', 5: 'foxglove'}
 
 
-def read_img(path):                                                    # 定义函数read_img，用于读取图像数据，并且对图像进行resize格式统一处理
-    cate=[path+x for x in os.listdir(path) if os.path.isdir(path+x)]  # 创建层级列表cate，用于对数据存放目录下面的数据文件夹进行遍历，os.path.isdir用于判断文件是否是目录，然后对是目录的文件(os.listdir(path))进行遍历
-    imgs=[]                                                            # 创建保存图像的空列表
-    labels=[]                                                          # 创建用于保存图像标签的空列表
-    for idx,folder in enumerate(cate):                                # enumerate函数用于将一个可遍历的数据对象组合为一个索引序列，同时列出数据和下标,一般用在for循环当中
-        for im in glob.glob(folder+'/*.jpg'):                         # 利用glob.glob函数搜索每个层级文件下面符合特定格式“/*.jpg”的图片，并进行遍历
-            #print('reading the images:%s'%(im))                      # 遍历图像的同时，打印每张图片的“路径+名称”信息
-            img=cv2.imread(im)                                        # 利用cv2.imread函数读取每一张被遍历的图像并将其赋值给img
-            img=cv2.resize(img,(w,h))                                 # 利用cv2.resize函数对每张img图像进行大小缩放，统一处理为大小为w*h的图像
-            imgs.append(img)                                          # 将每张经过处理的图像数据保存在之前创建的imgs空列表当中
-            labels.append(idx)                                        # 将每张经过处理的图像的标签数据保存在之前创建的labels列表当中
-    return np.asarray(imgs,np.float32),np.asarray(labels,np.int32)   # 利用np.asarray函数对生成的imgs和labels列表数据进行转化，之后转化成数组数据（imgs转成浮点数型，labels转成整数型）
+_ = Parameters()
 
 
-data, label = read_img(path)                                           # 将read_img函数处理之后的数据定义为样本数据data和标签数据label
-print("shape of data:",data.shape)                                     # 查看样本数据的大小
-print("shape of label:",label.shape)                                   # 查看标签数据的大小
+# API for reading image from disk
+def read_img(path, wide, height):
+    cate = [path + x for x in os.listdir(path) if os.path.isdir(path + x)]
+    imgs = []
+    labels = []
+
+    for idx, folder in enumerate(cate):
+        for im in glob.glob(folder + '/*.jpg'):
+            # print('reading the images:%s'%(im))
+            img = cv2.imread(im)
+            img = cv2.resize(img, (wide, height))
+            imgs.append(img)
+            labels.append(idx)
+
+    return np.asarray(imgs, np.float32), np.asarray(labels, np.int32)
 
 
-seed = 109             # 设置随机数种子，即seed值
-np.random.seed(seed)   # 保证生成的随机数具有可预测性,即相同的种子（seed值）所产生的随机数是相同的
+def preprocess():
+    data, label = read_img(_.path_train, _.wide, _.height)
+    print("shape of data:", data.shape)
+    print("shape of label:", label.shape)
 
-(x_train, x_val, y_train, y_val) = train_test_split(data, label, test_size=0.20, random_state=seed) #拆分数据集一部分为训练集一部分为验证集，拆分比例可调整
-x_train = x_train / 255  #训练集图片标准化
-x_val = x_val / 255      #测试集图片标准化
+    seed = 109
+    np.random.seed(seed)
 
-flower_dict = {0:'bee',1:'blackberry',2:'blanket',3:'bougainvillea',4:'bromelia',5:'foxglove'} #创建图像标签列表
+    (x_train, x_val, y_train, y_val) = train_test_split(
+        data, label, test_size=_.split, random_state=seed)
 
-# Todo 自行实现模型结构
-model = Sequential([])
+    # Standardize the data
+    x_val = x_val / 255
 
-# Todo 可调整超参数lr，可选择其他优化器
-opt = optimizers.Adam(learning_rate=0.0001)   #使用Adam优化器，优化模型参数。lr(learning rate, 学习率)
-
-#编译模型以供训练。使用多分类损失函数'sparse_categorical_crossentropy'，使用metrics=['accuracy']即评估模型在训练和测试时的性能的指标，使用的准确率。
-# Todo可选择其他损失函数
-model.compile(optimizer=opt,
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    return x_train, x_val, y_train, y_val
 
 
-# Todo 可调整超参数
-#训练模型，决定训练集和验证集，batch size：进行梯度下降训练模型时每个batch包含的样本数。
-#verbose：日志显示，0为不在标准输出流输出日志信息，1为输出进度条记录，2为每个epoch输出一行记录
-model.fit(x_train, y_train, epochs=30, validation_data=(x_val, y_val),batch_size=200, verbose=2)
-#输出模型的结构和参数量
-model.summary()
-model.save(model_path)  #保存模型
+def build():
+    model = Sequential([
+        Conv2D(16, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Conv2D(32, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Conv2D(64, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Dropout(0.5),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(6, activation="softmax")
+    ])
+
+    opt = Adam(learning_rate=_.lr)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+    return model
+
+
+def train(model, x_train, y_train, x_val, y_val):
+
+    image_gen_train = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True
+    )
+
+    image_gen_train.fit(x_train)
+
+    history = model.fit(image_gen_train.flow(x_train, y_train, batch_size=_.batch_size),
+                        validation_data=(x_val, y_val),
+                        epochs=_.epochs,
+                        steps_per_epoch=len(x_train) // _.batch_size,
+                        verbose=_.verbose,
+                        shuffle=True)
+
+    model.summary()
+
+    model.save(_.path_model)
+
+    # Draw the train and validation loss
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+    return model
+
+
+if __name__ == '__main__':
+    x_train, x_val, y_train, y_val = preprocess()
+
+    model = build()
+
+    model = train(model, x_train, y_train, x_val, y_val)
