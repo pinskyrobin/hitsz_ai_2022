33cf56c83cf10d70f8e8804f8cceccd61412b058
PinskyRobin <xu568059888@163.com>
2022-04-25 12:10:39 +0800

    using seved_model to predict

--- exp2/tensorflow/lab2_flower_classify.py
+++ exp2/tensorflow/lab2_flower_classify.py
@@ -12,20 +12,26 @@ from sklearn.model_selection import train_test_split
 
 # If following packages cannot be recognized,
 # please use "tensorflow.keras.xxx" instead
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.callbacks import EarlyStopping
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.callbacks import EarlyStopping
+
+# from tensorflow.python.keras.models import Sequential
+# from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+# from keras_preprocessing.image import ImageDataGenerator
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 
 # If following packages cannot be recognized,
-# please use please use "tensorflow.keras.optimizer" instead
-from tensorflow.python.keras.optimizer_v2.adam import Adam
-from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
-# from tensorflow.python.keras.optimizer.adam import Adam
-# from tensorflow.python.keras.optimizer.gradient_descent import SGD
+# please use please use "tensorflow.keras.optimizers" instead
+
+# from tensorflow.python.keras.optimizer_v2.adam import Adam
+
+from tensorflow.keras.optimizers import Adam
 
 # Fix prediction failure in macOS environment
 # If this line raise any error, please just COMMENT it
+
 tf.executing_eagerly()
 
 
@@ -36,15 +42,14 @@ class Parameters:
         cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
         self.path_train = os.path.join(cur_dir, 'flower_photos/')
         self.path_test = os.path.join(cur_dir, 'TestImages/')
-        self.wide = 224
-        self.height = 224
+        self.wide = 100
+        self.height = 100
         self.channel = 3
         self.lr = 0.001
-        self.epochs = 20
-        self.dropout = 0.5
-        self.batch_size = 64
-        self.split = 0.3
-        self.seed = 42
+        self.epochs = 90
+        self.batch_size = 32
+        self.split = 0.20
+        self.verbose = 2
 
         self.flower_dict = {0: 'bee', 1: 'blackberry', 2: 'blanket',
                             3: 'bougainvillea', 4: 'bromelia', 5: 'foxglove'}
@@ -75,43 +80,65 @@ def preprocess():
     print("shape of data:", data.shape)
     print("shape of label:", label.shape)
 
+    seed = 109
+    np.random.seed(seed)
+
     (x_train, x_val, y_train, y_val) = train_test_split(
-        data, label, test_size=_.split, random_state=_.seed)
+        data, label, test_size=_.split, random_state=seed)
 
     # Standardize the data
-    x_train = x_train / 255
     x_val = x_val / 255
 
     return x_train, x_val, y_train, y_val
 
 
 def build():
-    model = Sequential()
+    model = Sequential([
+        Conv2D(16, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Conv2D(32, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Conv2D(64, 3, padding='same', activation='relu'),
+        MaxPooling2D(),
+        Dropout(0.5),
+        Flatten(),
+        Dense(128, activation='relu'),
+        Dense(6, activation="softmax")
+    ])
+
+    opt = Adam(learning_rate=_.lr)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
     return model
 
 
 def train(model, x_train, y_train, x_val, y_val):
 
-    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=5, restore_best_weights=True)
+    image_gen_train = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=True
+    )
 
-    opt = Adam(learning_rate=_.lr)
-    # sgd = SGD(lr=_.lr, momentum=0.9, nesterov=True)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer=opt, metrics=['acc'])
+    image_gen_train.fit(x_train)
 
-    history = model.fit(x_train, y_train,
-                        epochs=_.epochs,
-                        callbacks=[es],
+    history = model.fit(image_gen_train.flow(x_train, y_train, batch_size=_.batch_size),
                         validation_data=(x_val, y_val),
-                        batch_size=_.batch_size,
-                        verbose=2)
+                        epochs=_.epochs,
+                        steps_per_epoch=len(x_train) // _.batch_size,
+                        verbose=_.verbose,
+                        shuffle=True)
 
     model.summary()
 
-    model.save_weights('flower_model.pkl')
+    model.save('flower_model.h5')
 
     # Draw the train and validation loss
-    acc = history.history['acc']
-    val_acc = history.history['val_acc']
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
     loss = history.history['loss']
     val_loss = history.history['val_loss']
     epochs = range(1, len(acc) + 1)
@@ -128,33 +155,37 @@ def train(model, x_train, y_train, x_val, y_val):
 
     return model
 
-def predict(model):
+
+def predict(_model=None, is_trained=False):
     imgs = []
 
     for im in glob.glob(_.path_test + '/*.jpg'):
-        print("====" + im + "====")
+        print('reading the images:%s' % (im))
         img = cv2.imread(im)
         img = cv2.resize(img, (_.wide, _.height))
         imgs.append(img)
     imgs = np.asarray(imgs, np.float32)
     print("shape of data:", imgs.shape)
 
+    model = tf.saved_model.load('model') if is_trained else _model
+
     prediction = np.argmax(model.predict(imgs), axis=1)
 
     # Draw the prediction
     for i in range(np.size(prediction)):
-        print("第", i + 1, "朵花预测:" + _.flower_dict[prediction[i]])
-        img = plt.imread(_.path_test + "test" + str(i + 1) +
-                         ".jpg")
+        print("第", i + 1, "朵花预测:", prediction[i])
+        img = plt.imread(_.path_test + "test" + str(i + 1) + ".jpg")
         plt.imshow(img)
         plt.show()
 
 
 if __name__ == '__main__':
-    x_train, x_val, y_train, y_val = preprocess()
+    # x_train, x_val, y_train, y_val = preprocess()
 
-    model = build()
+    # model = build()
 
-    model = train(model, x_train, y_train, x_val, y_val)
+    # model = train(model, x_train, y_train, x_val, y_val)
 
-    predict(model)
+    # predict(model)
+
+    predict(is_trained=True)
